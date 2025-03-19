b2751f48cd620e1b2b92ef664be5f72392f0336f
MiguelLewandowski <miguelstrapazonl@gmail.com>
2025-03-19 10:07:02 -0300

    :lipstick:feat:estilizacao do card dos dados do usuario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,26 @@
 
 import { useEffect, useState } from "react";
 import { fetchGitHubUser } from "@/services/github";
+import Image from "next/image";
 
 export default function RepositoriesPage() {
   const [user, setUser] = useState<any>(null);
-  
+
   useEffect(() => {
     fetchGitHubUser("RaulLize").then(setUser);
+    console.log(fetchGitHubUser("RaulLize"))
   }, []);
 
   return (
     <div>
-      <h1>Perfil do GitHub</h1>
       {user ? (
-        <div>
-          <img src={user.avatar_url} alt={user.login} width={100} />
-          <p>Nome: {user.name}</p>
-          <p>Bio: {user.bio}</p>
+        <div className="cardUser">
+          <Image src={user.avatar_url} className="rounded-full" alt={user.login} width={150} height={150} />
+          <div className="mt-2 text-center">
+            <p className="text-3xl font-semibold">{user.name}</p>
+            <p className="mt-1 text-gray-500">{user.bio}</p>
+          </div>
+
           <p>Repositórios públicos: {user.public_repos}</p>
         </div>
       ) : (
